a986bfa56b1c99d469b7eb40bd81100786dc823a
luvshah11 <luv.h.shah@gmail.com>
2021-05-20 05:46:54 -0500

    stdout to system.out.println
    fixing print calls.

--- MST_demo/src/minimalTrees/IndexMinPQ.java
+++ MST_demo/src/minimalTrees/IndexMinPQ.java
@@ -355,9 +355,9 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
         // delete and print each key
         while (!pq.isEmpty()) {
             int i = pq.delMin();
-            StdOut.println(i + " " + strings[i]);
+            System.out.println(i + " " + strings[i]);
         }
-        StdOut.println();
+        System.out.println();
 
         // reinsert the same strings
         for (int i = 0; i < strings.length; i++) {
