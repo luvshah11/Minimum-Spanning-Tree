043ec0aa183bafa8efaa34b677f12583c63f815b
= <dompace181@gmail.com>
2021-05-20 13:58:52 -0500

    EdgeWeight constructor input stream
    EdgeWeightedGraph(InputStream in)
    - initializes EWG from input stream with specific format
    <num_of_verts>
    <num_of_edges>
    <vert1> <vert2> <weight>
    <vert1> <vert2> <weight>
    <vert1> <vert2> <weight>
    
    changed some definitions.

--- MST_demo/src/minimalTrees/EdgeWeightedGraph.java
+++ MST_demo/src/minimalTrees/EdgeWeightedGraph.java
@@ -10,28 +10,71 @@ package minimalTrees;
 //+ edges():Iterable<Edge> all of this graph’s edges
 //+toString():String string representation
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class EdgeWeightedGraph implements Graph<Integer> {
 
-	private final int V; // number of vertices
+	private int V; // number of vertices
 	private int E; // number of edges
 	private Collection<Edge>[] adj; // adjacency lists
 
 	public EdgeWeightedGraph(Integer V) {
 		this.V = V;
 		this.E = 0;
-		adj = (Collection<Edge>[]) new Collection[V];
+		adj = new ArrayList[V];
 		for (int v = 0; v < V; v++)
 			adj[v] = new ArrayList<Edge>();
 	}
 
-//	public EdgeWeightedGraph(InputStream in)
-//	{
-//		
-//	}
+	
+    /*
+     *  INPUT FORMAT:
+     *  <num_of_verts>
+     *  <num_of_edges>
+     *  <vert1> <vert2> <weight>
+     *  <vert1> <vert2> <weight>
+     *  <vert1> <vert2> <weight>
+     *  ...
+     * 
+     */
+	public EdgeWeightedGraph(InputStream in) throws IOException
+	{
+		
+
+
+		 // https://stackoverflow.com/questions/309424/how-do-i-read-convert-an-inputstream-into-a-string-in-java
+		 String result = new BufferedReader(new InputStreamReader(in))
+				   .lines().collect(Collectors.joining("\n"));
+		 
+		
+		 String lines[] = result.split("\n");
+		 this.V = Integer.parseInt(lines[0]);
+		 this.E= Integer.parseInt(lines[1]);
+		 adj = new ArrayList[V];
+			for (int v = 0; v < V; v++)
+				adj[v] = new ArrayList<Edge>();
+		 
+		 
+		 for(int i = 2; i < lines.length; i++) {
+			 String cols[] = lines[i].split(" ");
+			 int v1 = Integer.parseInt(cols[0]);
+			 int v2 = Integer.parseInt(cols[1]);
+			 int w  = Integer.parseInt(cols[2]);
+			 Edge tmp = new Edge(v1, v2, w);
+			 adj[v1].add(tmp);
+			 adj[v2].add(tmp);
+			 E++;
+			 
+		 }
+		
+		
+	}
 	// See Exercise 4.3.9.
 	public int V() 
 	{
@@ -65,12 +108,12 @@ public class EdgeWeightedGraph implements Graph<Integer> {
 	@Override
 	public int getNumberOfVerticies()
 	{
-		return 0;
+		return this.V;
 	}
 	@Override
 	public int getNumberOfEdges()
 	{
-		return 0;
+		return this.E;
 	}
 
 	@Override
