32beb685de84e0bdc587a0bcb00c4ccae018c563
= <dompace181@gmail.com>
2021-05-25 11:25:34 -0500

    Updated EGW inputstream
    Added comments (// at start of line).

--- MST_demo/src/minimalTrees/EdgeWeightedGraph.java
+++ MST_demo/src/minimalTrees/EdgeWeightedGraph.java
@@ -56,6 +56,8 @@ public class EdgeWeightedGraph{
 		 
 		 for(int i = 2; i < lines.length; i++) {
 			 String cols[] = lines[i].split(" ");
+			 if(cols[0] == "//")
+				 continue;
 			 int v1 = Integer.parseInt(cols[0]);
 			 int v2 = Integer.parseInt(cols[1]);
 			 double w  = Double.parseDouble(cols[2]);
