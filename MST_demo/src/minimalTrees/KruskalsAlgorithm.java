ea6a513e12b906416db8f5973e73b5ca984aa199
luvshah11 <luv.h.shah@gmail.com>
2021-05-30 01:00:42 -0500

    Finalized build
    -Print statment for graph wights
    -fixed prim cta bug : Needed to correct the data in cta.txt: the nodes should start at 0, not 1. the # of edges is less than nodes, not -2 less

--- MST_demo/src/minimalTrees/KruskalsAlgorithm.java
+++ MST_demo/src/minimalTrees/KruskalsAlgorithm.java
@@ -1,7 +1,6 @@
 	package minimalTrees;
 
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
--- MST_demo/src/minimalTrees/demoMain.java
+++ MST_demo/src/minimalTrees/demoMain.java
@@ -1,19 +1,18 @@
 package minimalTrees;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Scanner;
 
 public class demoMain {
 	public static void main(String[] args ) throws IOException{
-		InputStream demoInput = new FileInputStream("C:\\Users\\User\\Documents\\GitHub\\Minimum-Spanning-Tree\\MST_demo\\demoinput.txt");
-		InputStream ctaInput = new FileInputStream("C:\\Users\\User\\Documents\\GitHub\\Minimum-Spanning-Tree\\MST_demo\\cta.txt");
+		InputStream demoInput = new FileInputStream("demoinput.txt");
+		InputStream ctaInput = new FileInputStream("cta.txt");
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to MST Demo");
 		EdgeWeightedGraph graphDemo = new EdgeWeightedGraph(demoInput);		
-		System.out.println(graphDemo.toString());
+		System.out.print(graphDemo.toString());
+		System.out.println("Weight of Edge Weighted Graph: " + graphDemo.getWeight() + "\n");
 
 
 		///Implement Prim's Algorithm -
@@ -21,27 +20,39 @@ public class demoMain {
 		//Include a Printing function to display what's in the class. 
 		System.out.println("Prim's algorithm");
 		PrimsAlgorithm prims = new PrimsAlgorithm(graphDemo);
-		System.out.println(prims.toString());
+		System.out.print(prims.toString());
+		System.out.println("Weight of Prim's MST: " + prims.weight() + "\n");
+		
 		///Implement Kurskal's Algorithm -
 		//same as 1. but with Kruskal.
 		System.out.println("Kruskal's algorithm");
 		KruskalsAlgorithm kruskals = new KruskalsAlgorithm(graphDemo);
-		System.out.println(kruskals.toString());
+		System.out.print(kruskals.toString());
+		System.out.println("Weight of Edge Weighted Graph: " + kruskals.weight() + "\n"); 
+		
 		///Generate a CTA edge wieghted graph-
 		//Weights are the total time spent sitting in the train. 
 		EdgeWeightedGraph ctaMap = new EdgeWeightedGraph(ctaInput);
-		System.out.println(ctaMap.toString());
+		System.out.print(ctaMap.toString());
+		System.out.println("CTA Map weight: " + ctaMap.getWeight() + "\n");
+		PrimsAlgorithm CTAprims = new PrimsAlgorithm(ctaMap);
+		System.out.print(CTAprims.toString());
+		System.out.println("Weight of Prim's CTA MST: " + CTAprims.weight() + "\n");
 		KruskalsAlgorithm CTAkruskals = new KruskalsAlgorithm(ctaMap);
-		System.out.println(CTAkruskals.toString());
+		System.out.print(CTAkruskals.toString());
+		System.out.println("Weight of Kruskal's CTA MST:" + CTAkruskals.weight());
 		
 		
 		
 		//graph tests: 
-		//the graph demo weight is 1.84, see attatched image DemoInput.png
+		//the graph demo weight is 1.84, see attached image DemoInput.png
 		MSTTest demoTest = new MSTTest(); 
 		demoTest.testKruskal(graphDemo, 1.84);
 		demoTest.testPrimms(graphDemo, 1.84);
+		
 		MSTTest CTATest = new MSTTest();
+		CTATest.testKruskal(ctaMap, 0);
+		CTATest.testPrimms(ctaMap, 0);
 	}
 
 }
